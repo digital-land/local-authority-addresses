cf5b4b5e719f8b3e330c8c82a6ea5d5beced2869
Paul Downey <psd@whatfettle.com>
2020-07-17 21:36:16 +0100

    ONSUD/PD comparison refactoring

--- bin/uprn-count.py
+++ bin/uprn-count.py
@@ -23,10 +23,12 @@ if __name__ == "__main__":
     for row in csv.DictReader(open("data/uprn.csv")):
         total["all"] += 1
 
+        # only look at BPLUs with ONSUD entry
         if row["ONSUD"] == "":
             total["missing-onsud"] += 1
             continue
 
+        # skip addresses outside of England
         if row["ONSUD"][0] != "E":
             if row["ONSPD"][0:1] == "E":
                 total["adopted"] += 1
@@ -34,17 +36,15 @@ if __name__ == "__main__":
 
         total["england"] += 1
 
+        # compare ONSUD and ONSPD
         if row["ONSPD"] == "":
             total["missing-onspd"] += 1
-            continue
-
-        if row["ONSUD"] == row["ONSPD"]:
+        elif row["ONSUD"] == row["ONSPD"]:
             total["same"] += 1
-            continue
+        else:
+            total["different"] += 1
 
-        total["different"] += 1
-
-        if row["ONSUD"][0:1] != row["ONSPD"][0:1]:
-            total["exiled"] += 1
+            if row["ONSUD"][0:1] != row["ONSPD"][0:1]:
+                total["exiled"] += 1
 
     print(json.dumps(total, sort_keys=True, indent=4))
